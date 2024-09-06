6e9ccb54562a3cd88bbcf652e3106902d6051e6a
Daniel <danieldimitrov2304@gmail.com>
2024-09-06 10:05:57 +0300

    Remove deprecated jws methods

--- UserService/user-service/src/main/java/com/example/userservice/config/SecurityConfig.java
+++ UserService/user-service/src/main/java/com/example/userservice/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -15,11 +16,11 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests(authorizeRequests ->
+                .authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests
                                 .anyRequest().permitAll()  // Allow all requests without authentication
                 )
-                .csrf(csrf -> csrf.disable());  // Disable CSRF for simplicity
+                .csrf(AbstractHttpConfigurer::disable);  // Disable CSRF for simplicity
 
         return http.build();
     }
--- UserService/user-service/src/main/java/com/example/userservice/util/JwtUtil.java
+++ UserService/user-service/src/main/java/com/example/userservice/util/JwtUtil.java
@@ -3,8 +3,10 @@ package com.example.userservice.util;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
 
+import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -14,6 +16,7 @@ import java.util.function.Function;
 public class JwtUtil {
 
     private final String SECRET_KEY = "0x5A10ee08e35B699E8c46c6DE92a82e135DCbb5d3df3"; // Keep this key safe and secure
+    private  final Key KEY = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
 
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
@@ -25,7 +28,7 @@ public class JwtUtil {
     }
 
     private Claims extractAllClaims(String token) {
-        return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
+        return Jwts.parserBuilder().setSigningKey(KEY).build().parseClaimsJws(token).getBody();
     }
 
     public Boolean isTokenExpired(String token) {
@@ -46,8 +49,8 @@ public class JwtUtil {
                 .setClaims(claims)
                 .setSubject(subject)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 10 hours expiration
-                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .setExpiration(new Date(System.currentTimeMillis() + 2 * 7 * 24 * 60 * 60 * 1000)) // 2 weeks
+                .signWith(KEY, SignatureAlgorithm.HS256)
                 .compact();
     }
 
