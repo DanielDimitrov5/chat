f42613eba18d9c379e75d3497bceced10e397d8b
Daniel <danieldimitrov2304@gmail.com>
2024-09-06 10:49:12 +0300

    User service (#4)
    * Initial commit: user service
    
    * Add jwt login
    
    * Remove depriecated methods
    
    * initial: chat-service

--- UserService/user-service/src/main/java/com/example/userservice/service/UserService.java
+++ UserService/user-service/src/main/java/com/example/userservice/service/UserService.java
@@ -2,9 +2,11 @@ package com.example.userservice.service;
 
 import com.example.userservice.model.User;
 import com.example.userservice.repository.UserRepository;
+import com.example.userservice.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -13,7 +15,15 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public User saveUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword())); // Encode password before saving
+
         return userRepository.save(user);
     }
 
@@ -23,5 +33,18 @@ public class UserService {
 
     public Optional<User> findById(Long id) {
         return userRepository.findById(id);
+    }
+
+    public String loginUser(String username, String password) {
+        Optional<User> user = userRepository.findByUsername(username);
+
+        if (user.isPresent() && passwordEncoder.matches(password, user.get().getPassword())) {
+            return jwtUtil.generateToken(username); // Return JWT token
+        }
+        return null; // Invalid credentials
+    }
+
+    public List<User> searchUsers(String username) {
+        return userRepository.findByUsernameContaining(username);
     }
 }
---
+++ UserService/user-service/src/main/java/com/example/userservice/util/JwtUtil.java
@@ -0,0 +1,61 @@
+package com.example.userservice.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Component
+public class JwtUtil {
+
+    private final String SECRET_KEY = "0x5A10ee08e35B699E8c46c6DE92a82e135DCbb5d3df3"; // Keep this key safe and secure
+    private  final Key KEY = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder().setSigningKey(KEY).build().parseClaimsJws(token).getBody();
+    }
+
+    public Boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    public Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    public String generateToken(String username) {
+        Map<String, Object> claims = new HashMap<>();
+        return createToken(claims, username);
+    }
+
+    private String createToken(Map<String, Object> claims, String subject) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(subject)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 2 * 7 * 24 * 60 * 60 * 1000)) // 2 weeks
+                .signWith(KEY, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public Boolean validateToken(String token, String username) {
+        final String extractedUsername = extractUsername(token);
+        return (extractedUsername.equals(username) && !isTokenExpired(token));
+    }
+}
